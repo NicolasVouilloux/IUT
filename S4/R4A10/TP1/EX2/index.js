aa9bf418717105995f280b370b88f778a44dd07f
Nicolas Vouilloux <nicolas.vouilloux@universite-paris-saclay.fr>
2025-02-14 11:53:38 +0100

    oui

--- S4/R4A10/TP1/EX2/index.js
+++ S4/R4A10/TP1/EX2/index.js
@@ -1,21 +1,7 @@
-let aside = document.getElementById("asideIndex");
-let asideButton = document.getElementById("asideButton");
+let tableau = document.querySelector('table')
+let tkt = tableau.addEventListener('click',change_couleur)
 
-
-function rajoute_un_clic()
+function change_couleur()
 {
-    if (asideButton.innerText === "Hello !"){
-        asideButton.innerText = "Bien ou quoi ?"
-        alert("OUI TA CLICKER SUR MOOI")
-    }
-    else alert("PLUS JAMAIS TU ME CLICK DESSUS T FOU ?")
-}
 
-function rajoute_un_clic_alt(that)
-{
-    if (that.innerText === "Hello !"){
-        that.innerText = "Bien ou quoi ?"
-        alert("OUI TA CLICKER SUR MOOI")
-    }
-    else alert("PLUS JAMAIS TU ME CLICK DESSUS T FOU ?")
 }
