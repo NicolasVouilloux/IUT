f0e6d299194694c36762d3344f78c674d8072847
Nicolas Vouilloux <nicolas.vouilloux@universite-paris-saclay.fr>
2025-02-14 10:01:01 +0100

    ONB COMMENCE LEXO DE JAVASCRIPT

---
+++ S4/R4A10/TP1/index.js
@@ -0,0 +1,11 @@
+let aside = document.getElementById("asideIndex");
+let asideButton = document.getElementById("asideButton");
+
+
+function rajoute_un_clic(){
+     if (asideButton.innerText === "Hello !"){
+        asideButton.innerText = "Bien ou quoi ?"
+     
+    }
+
+}
