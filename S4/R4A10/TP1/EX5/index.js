faccdc9f6cef66ab4f2bc5783d9c5a22a22959ca
Nicolas Vouilloux <nicolasvcire@gmail.com>
2025-03-28 10:15:06 +0100

    all good starting ex5

---
+++ S4/R4A10/TP1/EX5/index.js
@@ -0,0 +1,53 @@
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    let tableau = document.querySelector('table')
+    let tkt = tableau.addEventListener('click',change_couleur)
+}) 
+
+
+function change_couleur(event)
+{
+    let color = event.target.innerText
+    let docText = document.querySelectorAll('p')
+    docText.forEach((element)=>{
+        element.style.color = color
+    })
+}
+
+function rajoute_un_deux_couleurs(){
+     let couleur1 = document.getElementById("ColorSelect1").value;
+     let couleur2 = document.getElementById("ColorSelect2").value;
+ 
+     let newRow = document.createElement("tr");
+ 
+     let cell1 = document.createElement("td");
+     cell1.style.backgroundColor = couleur1;
+     cell1.textContent = couleur1;
+ 
+     let cell2 = document.createElement("td");
+     cell2.style.backgroundColor = couleur2;
+     cell2.textContent = couleur2;
+ 
+     newRow.appendChild(cell1);
+     newRow.appendChild(cell2);
+ 
+     document.querySelector("tbody").appendChild(newRow);
+}
+
+let paragraphOriginal = document.querySelectorAll("p");
+
+function recherche_surligne(event) {
+    let input = document.getElementById("site-search").value;
+
+    paragraphOriginal.forEach(p => {
+        p.innerHTML = p.textContent;
+    });
+
+   
+    if (input.length > 0) {
+        paragraphOriginal.forEach(p => {
+            let regex = new RegExp(`(${input})`, 'gi');
+            p.innerHTML = p.textContent.replace(regex, '<span class="highlight">$1</span>');
+        });
+    }
+}
